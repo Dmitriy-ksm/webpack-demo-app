f500498705d4b5cd8f5c4f13bd40b171017492ce
dimadim4ik <dimadim4ik@gmail.com>
2021-11-06 00:19:54 +0200

    int/p install, sass/ccs/style l, pl for clear dist

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+const path=require("path");
+
+module.exports = {
+    mode:"development",
+    entry:"./src/index.js",
+    output:{
+        filename:"main.[contenthash].js",
+        path:path.resolve(__dirname,"dist")
+    },
+    plugins:[new HtmlWebpackPlugin({
+        template:"./src/template.html"
+    })],
+    module:{
+        rules:[
+            {
+                test: /\.scss$/,
+                use:[
+                    "style-loader", // js(css) to dom
+                    "css-loader", //2. css to js
+                    "sass-loader" //1.sass to css
+                ]
+            }
+        ]
+    }
+}
